1d8772a2496c141447f3b72f30ffd8b239c651c0
Turbobolekus <adam01wroblewski@gmail.com>
2024-03-15 17:21:39 +0100

    add mcp9808 sensor control

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,28 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <Adafruit_MCP9808.h>
 
 
 #define channel_id 2463135
 #define write_api_key "V5HXBBRF8G4HKIDI"
 WiFiClient wifi_client;
 
-#define wifi_ssid "Orange_Gosc_7AD0"
-#define wifi_password "dupadupa"
+// #define wifi_ssid "Orange_Gosc_7AD0"
+// #define wifi_password "dupadupa"
+#define wifi_ssid "zetor"
+#define wifi_password "ursus360"
 #define wifi_timeout 60000
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BME280 bme; // I2C
-float temperature;
-float humidity;
-float pressure;
-float altitude;
+float bme_temperature;
+float bme_humidity;
+float bme_pressure;
+float bme_altitude;
 
+Adafruit_MCP9808  mcp;
+float mcp_temperature;
 
 unsigned long rain_last_impulse_millis = 0;
 unsigned long rain_interval = 900000;
@@ -62,36 +67,46 @@ void initWiFi()
 }
 
 
-void printBMEvalues() {
+void print_values() {
     Serial.println();
+    Serial.print("MCP TEMP = ");
+    Serial.print(mcp_temperature);
+    Serial.println(" *C");
+
     Serial.print("Temperature = ");
-    Serial.print(temperature);
+    Serial.print(bme_temperature);
     Serial.println(" *C");
 
     Serial.print("Pressure = ");
-    Serial.print(pressure);
+    Serial.print(bme_pressure);
     Serial.println(" hPa");
 
     Serial.print("Approx. Altitude = ");
-    Serial.print(altitude);
+    Serial.print(bme_altitude);
     Serial.println(" m");
 
     Serial.print("Humidity = ");
-    Serial.print(humidity);
+    Serial.print(bme_humidity);
     Serial.println(" %");
     
     Serial.print("RainFall = ");
     Serial.print(rainfall_value);
     Serial.println(" mm/m^2");
-
 }
 
 
 void getBMEvalues() {
-    temperature = bme.readTemperature();
-    pressure = bme.readPressure() / 100.0F;
-    altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-    humidity = bme.readHumidity();
+    bme_temperature = bme.readTemperature();
+    bme_pressure = bme.readPressure() / 100.0F;
+    bme_altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+    bme_humidity = bme.readHumidity();
+}
+
+void getMCPvalues()
+{
+    mcp.wake();
+    mcp_temperature = mcp.readTempC();
+    mcp.shutdown_wake(1);
 }
 
 
@@ -140,6 +155,15 @@ void setup() {
         Serial.println("BME280 detected");
         Serial.println();
     }   
+
+    if (!mcp.begin(0x18)) {
+        Serial.println("Could not find a valid MCP9808 sensor, check wiring!");
+    }
+    else{
+        Serial.println("MCP9808 detected");
+        Serial.println();
+        mcp.printSensorDetails();
+    }
 }
 
 
@@ -156,30 +180,31 @@ void loop() {
         ESP.restart();
     }
     
-    if(WiFi.localIP().toString() == "192.168.144.3") {
+    if(WiFi.localIP().toString() == "192.168.10.201") {
         digitalWrite(LED_BUILTIN, HIGH);
         
         unsigned long upload_current_millis = millis();
 
-        rainMeter();
+        // rainMeter();
         // delay(10);
 
         if(millis() - upload_previousMillis > 15000)
         {
             getBMEvalues();
-            printBMEvalues();
-            ThingSpeak.setField(1, temperature);
-            ThingSpeak.setField(2, pressure);
-            ThingSpeak.setField(3, humidity);
-            ThingSpeak.setField(4, rainfall_value);
+            getMCPvalues();
+            print_values();
+            // ThingSpeak.setField(1, bme_temperature);
+            // ThingSpeak.setField(2, bme_pressure);
+            // ThingSpeak.setField(3, bme_humidity);
+            // ThingSpeak.setField(4, rainfall_value);
             
-            ThingSpeak.setField(5, rain_indicator);
+            // ThingSpeak.setField(5, rain_indicator);
             
-            ThingSpeak.writeFields(channel_id,write_api_key);
-            Serial.println("SENT DATA TO CLOUD");
+            // ThingSpeak.writeFields(channel_id,write_api_key);
+            // Serial.println("SENT DATA TO CLOUD");
 
             upload_previousMillis = millis();
         }
-    }
+    }z
 }
 
