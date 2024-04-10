a93dfbd92126af2647a0f026207bd8569fbca314
Turbobolekus <adam01wroblewski@gmail.com>
2024-04-10 12:28:31 +0200

    add yl-83 sensor

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,23 @@
 #include <Adafruit_BME280.h>
 #include <Adafruit_MCP9808.h>
 
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSerial.h>
 
 #define channel_id 2463135
 #define write_api_key "V5HXBBRF8G4HKIDI"
 WiFiClient wifi_client;
 
+AsyncWebServer server(80);
+
 // #define wifi_ssid "Orange_Gosc_7AD0"
 // #define wifi_password "dupadupa"
-#define wifi_ssid "zetor"
-#define wifi_password "ursus360"
+#define wifi_ssid "Orange_Swiatlowod_7AD0"
+#define wifi_password "7adkKdbCs7PHKp4ea3"
+
+// #define wifi_ssid "zetor"
+// #define wifi_password "ursus360"
 #define wifi_timeout 60000
 
 #define SEALEVELPRESSURE_HPA (1013.25)
@@ -27,6 +35,9 @@ float bme_altitude;
 
 Adafruit_MCP9808  mcp;
 float mcp_temperature;
+
+const int yl_digital_pin = 16;
+const int yl_analog_pin = 4 ;
 
 unsigned long rain_last_impulse_millis = 0;
 unsigned long rain_interval = 900000;
@@ -40,7 +51,9 @@ bool prev_impulse_val = false;
 
 
 unsigned long upload_previousMillis = 0;
-unsigned long upload_interval = 60000;
+unsigned long upload_interval = 1500;
+String ip_address = "192.168.1.30";
+float offset = 0.0;
 
 void initWiFi()
 {
@@ -94,6 +107,43 @@ void print_values() {
     Serial.println(" mm/m^2");
 }
 
+void print_web_values() {
+    WebSerial.println();
+    WebSerial.print("MCP temp = ");
+    WebSerial.print(mcp_temperature);
+    WebSerial.print(" *C");
+    WebSerial.print(" | ");
+    delay(100);
+    
+
+    WebSerial.print("MBE temp = ");
+    WebSerial.print(bme_temperature);
+    WebSerial.print(" *C");
+    WebSerial.print(" | ");
+    delay(100);
+
+    // WebSerial.print("Pre = ");
+    WebSerial.print(bme_pressure);
+    WebSerial.print(" hPa");
+    WebSerial.print(" | ");
+    delay(100);
+
+    // // WebSerial.print("Approx. Altitude = ");
+    // WebSerial.print(bme_altitude);
+    // WebSerial.print(" m");
+    // WebSerial.print(" | ");
+    // delay(100);
+
+    // WebSerial.print("Humidity = ");
+    WebSerial.print(bme_humidity);
+    WebSerial.print(" %");
+    delay(100);
+    
+    // WebSerial.print("RainFall = ");
+    // WebSerial.print(rainfall_value);
+    // WebSerial.println(" mm/m^2");
+}
+
 
 void getBMEvalues() {
     bme_temperature = bme.readTemperature();
@@ -102,11 +152,12 @@ void getBMEvalues() {
     bme_humidity = bme.readHumidity();
 }
 
+
 void getMCPvalues()
 {
-    mcp.wake();
-    mcp_temperature = mcp.readTempC();
-    mcp.shutdown_wake(1);
+    // mcp.wake();
+    mcp_temperature = mcp.readTempC() - offset;
+    // mcp.shutdown_wake(1);
 }
 
 
@@ -140,13 +191,52 @@ void rainMeter()
     }
 }
 
+
+void recvMsg(uint8_t *data, size_t len){
+    WebSerial.println("Received Data...");
+    String d = "";
+    for(int i=0; i < len; i++){
+        d += char(data[i]);
+    }
+    WebSerial.println(d);
+    if (d == "reset"){
+        Serial.println("web reset");
+        WebSerial.println("web reset");
+        delay(2000);
+        ESP.restart();
+    }
+    if (d.toFloat()){
+        offset = d.toFloat();
+        WebSerial.println("New offset value ");
+        WebSerial.print(offset);
+        Serial.println("New offset value ");
+        Serial.print(offset);
+    }
+}
+
+void send_data_to_cloud()
+{
+    ThingSpeak.setField(1, mcp_temperature);
+    ThingSpeak.setField(2, bme_pressure);
+    ThingSpeak.setField(3, bme_humidity);
+    // ThingSpeak.setField(4, rainfall_value);
+    // ThingSpeak.setField(5, rain_indicator);
+    
+    ThingSpeak.writeFields(channel_id,write_api_key);
+    Serial.println("SENT DATA TO CLOUD");
+}
+
 void setup() {
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
     
-    initWiFi();
+    // initWiFi();
 
-    ThingSpeak.begin(wifi_client);
+    // ThingSpeak.begin(wifi_client);
+
+    // WebSerial.begin(&server);
+    // WebSerial.msgCallback(recvMsg);
+    // server.begin();
     
     if (!bme.begin(0x76)) {
         Serial.println("Could not find a valid BME280 sensor, check wiring!");
@@ -168,19 +258,15 @@ void setup() {
 
 
 void loop() {
-    // Serial.println(WiFi.localIP());
-    // Serial.print("wifi status: ");
-    // Serial.println(WiFi.status());
-    // Serial.println();
     digitalWrite(LED_BUILTIN, WiFi.status() == WL_CONNECTED);
     
-    if(WiFi.localIP().toString() == "0.0.0.0") {
-        digitalWrite(LED_BUILTIN, LOW);
-        Serial.println("Connection lost...");
-        ESP.restart();
-    }
+    // if(WiFi.localIP().toString() == "0.0.0.0") {
+    //     digitalWrite(LED_BUILTIN, LOW);
+    //     Serial.println("Connection lost...");
+    //     ESP.restart();
+    // }
     
-    if(WiFi.localIP().toString() == "192.168.10.201") {
+    // if(WiFi.localIP().toString() == ip_address) {
         digitalWrite(LED_BUILTIN, HIGH);
         
         unsigned long upload_current_millis = millis();
@@ -188,23 +274,22 @@ void loop() {
         // rainMeter();
         // delay(10);
 
-        if(millis() - upload_previousMillis > 15000)
+        if(millis() - upload_previousMillis > upload_interval)
         {
             getBMEvalues();
             getMCPvalues();
             print_values();
-            // ThingSpeak.setField(1, bme_temperature);
-            // ThingSpeak.setField(2, bme_pressure);
-            // ThingSpeak.setField(3, bme_humidity);
-            // ThingSpeak.setField(4, rainfall_value);
-            
-            // ThingSpeak.setField(5, rain_indicator);
-            
-            // ThingSpeak.writeFields(channel_id,write_api_key);
-            // Serial.println("SENT DATA TO CLOUD");
-
+            // print_web_values();
+            // send_data_to_cloud();
+            Serial.print("rain digital: ");
+            Serial.println(digitalRead(yl_digital_pin));
+            Serial.print("rain analog: ");
+            Serial.println(analogRead(yl_analog_pin));
+            Serial.println();
+            Serial.println();
+            Serial.println();
             upload_previousMillis = millis();
         }
-    }
+    // }
 }
 
