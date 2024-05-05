154697349b715697a4227cdab505e904022a6b2a
Turbobolekus <adam01wroblewski@gmail.com>
2024-05-05 17:59:58 +0200

    clean code

--- src/main.cpp
+++ src/main.cpp
@@ -46,22 +46,12 @@ Adafruit_MCP9808  mcp;
 float mcp_temperature;
 
 const int yl_digital_pin = 16;
-const int yl_analog_pin = 4;
+const int yl_analog_pin = 32;
 float rain_analog;
 bool rain = false;
 
-unsigned long rain_last_impulse_millis = 0;
-unsigned long rain_interval = 900000;
-float mm_per_tick = 0.217;
-float rainfall_value = 0;
-float rain_indicator = 0;
-int impulse_counter = 0;
-bool impulse = false;
-bool prev_impulse_val = false;
-
-
 unsigned long last_upload_time = 0;
-unsigned long upload_interval = 15000;
+unsigned long upload_interval = 100;
 String ip_address = "192.168.10.201";
 float offset = 0.0;
 
@@ -112,8 +102,7 @@ void print_values() {
     Serial.println(" %");
     
     Serial.print("RainFall = ");
-    Serial.print(rainfall_value);
-    Serial.println(" mm/m^2");
+    Serial.println(rain);
 }
 
 void print_web_values() {
@@ -124,83 +113,25 @@ void print_web_values() {
     WebSerial.print(" | ");
     delay(100);
     
-
     WebSerial.print("MBE temp = ");
     WebSerial.print(bme_temperature);
     WebSerial.print(" *C");
     WebSerial.print(" | ");
     delay(100);
 
-    // WebSerial.print("Pre = ");
     WebSerial.print(bme_pressure);
     WebSerial.print(" hPa");
     WebSerial.print(" | ");
     delay(100);
 
-    // // WebSerial.print("Approx. Altitude = ");
-    // WebSerial.print(bme_altitude);
-    // WebSerial.print(" m");
-    // WebSerial.print(" | ");
-    // delay(100);
-
-    // WebSerial.print("Humidity = ");
     WebSerial.print(bme_humidity);
     WebSerial.print(" %");
+    WebSerial.print(" | ");
     delay(100);
-    
-    // WebSerial.print("RainFall = ");
-    // WebSerial.print(rainfall_value);
-    // WebSerial.println(" mm/m^2");
-}
 
-void getBMEvalues() {
-    bme_temperature = bme.readTemperature();
-    bme_pressure = bme.readPressure() / 100.0F;
-    bme_altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-    bme_humidity = bme.readHumidity();
-}
-
-void getMCPvalues()
-{
-    // mcp.wake();
-    mcp_temperature = mcp.readTempC() - offset;
-    // mcp.shutdown_wake(1);
-}
-
-void getYLvalues(){
-    // rain = digitalRead(yl_digital_pin);
-    rain_analog = analogRead(yl_analog_pin);
-    rain_analog < 2200 ? rain = true : rain = false; 
-}
-
-void rainMeter()
-{
-    prev_impulse_val = impulse;
-    impulse = (analogRead(32) < 2350) ? true : false;
-    while(impulse)
-    {
-        prev_impulse_val = impulse;
-        impulse = (analogRead(32) < 2350) ? true : false;
-        // Serial.println("waiting");
-    }
-    if(prev_impulse_val == 1 && impulse == 0) 
-    {
-        Serial.println("impulse");
-        Serial.println();
-        delay(20);
-        impulse_counter = impulse_counter + 1;
-        rainfall_value = impulse_counter * mm_per_tick;
-        
-        rain = true;
-        rain_indicator = 1;
-        rain_last_impulse_millis = millis();
-    }
-    if(rain = true && millis() - rain_last_impulse_millis > rain_interval)
-    {
-        rain = false;
-        rain_indicator = 0;
-        rainfall_value = 0;
-    }
+    WebSerial.print(rain);
+    WebSerial.print(" rain");
+    delay(100);
 }
 
 void recvMsg(uint8_t *data, size_t len){
@@ -225,25 +156,42 @@ void recvMsg(uint8_t *data, size_t len){
     }
 }
 
-void MQTTreconnect() {
-  // Loop until we're reconnected
-  while (!MQTT_client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-      //client.connect("cliend ID", "username","password") Replace with your Thingspeak MQTT Device Credentials
-    if (MQTT_client.connect(SECRET_MQTT_USERNAME, SECRET_MQTT_CLIENT_ID, SECRET_MQTT_PASSWORD)) {  
-      Serial.println("connected");
-    //   MQTT_client.subscribe(subscribeTopicFor_Command_1);   // subscribe the topics here
-      //client.subscribe(command2_topic);   
-      
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(MQTT_client.state());
-      Serial.println(" try again in 5 seconds");   // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
+void getBMEvalues() {
+    bme_temperature = bme.readTemperature();
+    bme_pressure = bme.readPressure() / 100.0F;
+    bme_altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+    bme_humidity = bme.readHumidity();
 }
 
+void getMCPvalues()
+{
+    mcp_temperature = mcp.readTempC() - offset;
+}
+
+void getYLvalues(){
+    rain_analog = analogRead(yl_analog_pin);
+    Serial.println(rain_analog);
+    rain_analog < 2200 ? rain = true : rain = false; 
+}
+
+
+void MQTTreconnect() {
+    // Loop until we're reconnected
+    while (!MQTT_client.connected()) {
+        Serial.println("Connecting with MQTT broker...");
+        if (MQTT_client.connect(SECRET_MQTT_USERNAME, SECRET_MQTT_CLIENT_ID, SECRET_MQTT_PASSWORD)) 
+        {  
+            Serial.println("connected");
+        } 
+        else 
+        {
+            Serial.println("failed connecting to MQTT broker, ");
+            Serial.print(MQTT_client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
 
 void send_data_to_cloud_https()
 {
@@ -272,21 +220,13 @@ void send_data_to_cloud_MQTT()
 }
 
 
-
-
-
 void setup() {
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
+    
     MQTT_client.setServer(mqtt_server, 1883);
     initWiFi();
-
-    // ThingSpeak.begin(wifi_client);
-
-    // WebSerial.begin(&server);
-    // WebSerial.msgCallback(recvMsg);
-    // server.begin();
-    
+   
     if (!bme.begin(0x76)) {
         Serial.println("Could not find a valid BME280 sensor, check wiring!");
     }
@@ -314,41 +254,27 @@ void loop() {
         Serial.println("Connection lost...");
         ESP.restart();
     }
-
-
-    if(WiFi.localIP().toString() == ip_address) {
+    else{
         digitalWrite(LED_BUILTIN, HIGH);
     
-        if(!MQTT_client.connected()) MQTTreconnect();    
-        MQTT_client.loop(); 
-
-
-        // rainMeter();
-        // delay(10);
-
-        if(millis() - last_upload_time > upload_interval)
+        if(!MQTT_client.connected()) MQTTreconnect();
+        else
         {
-            // getBMEvalues();
-            // getMCPvalues();
-            // mcp_temperature = random(100);
-            // bme_humidity = random(100);
-            // bme_pressure = random(100);
-            rain = !rain;
+            MQTT_client.loop(); 
 
-            print_values();
-            // print_web_values();
-            // send_data_to_cloud_https();
-            send_data_to_cloud_MQTT();
+            if(millis() - last_upload_time > upload_interval)
+            {
+                getBMEvalues();
+                getMCPvalues();
+                getYLvalues();
 
-            Serial.print("rain digital: ");
-            Serial.println(digitalRead(yl_digital_pin));
-            Serial.print("rain analog: ");
-            Serial.println(analogRead(yl_analog_pin));
-            Serial.println();
-            Serial.println();
-            Serial.println();
-            last_upload_time = millis();
+                print_values();
+                
+                send_data_to_cloud_MQTT();
+            
+                last_upload_time = millis();
+            }
         }
-    }
+    }    
 }
 
